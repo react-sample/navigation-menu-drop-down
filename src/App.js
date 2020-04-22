9d3a692802b6a542435c9ed657b59f9d18c04e47
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-22 17:43:57 +0700

    add navItem

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 
+import './index.css';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+
 function App() {
   return (
-    <Navbar></Navbar>
+    <Navbar>
+      <NavItem icon={<PlusIcon />}/>
+      <NavItem icon={<BellIcon />}/>
+      <NavItem icon={<MessengerIcon />}/>
+    </Navbar>
   );
 }
 
-function Navbar() {
+function Navbar({children}) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav"></ul>
+      <ul className="navbar-nav">{children}</ul>
     </nav>
+  );
+}
+
+function NavItem({icon}) {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {icon}
+      </a>
+    </li>
   );
 }
 
